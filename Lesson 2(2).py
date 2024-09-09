11f3a7bccce2382ceae0eae15ce241ac07d249cb
Askar4272 <askar4272@gmail.com>
2024-09-09 21:12:20 +0500

    Add files via upload

---
+++ Lesson 2(2).py
@@ -0,0 +1,8 @@
+first = input('введите число: ')
+second = input('введите число: ')
+third = input('введите число: ')
+if first == second and second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else: print(0)
